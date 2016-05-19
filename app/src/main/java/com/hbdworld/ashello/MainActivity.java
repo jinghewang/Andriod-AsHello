2de21a997fc0740b63b9995e63e4df1bbd4b45ad
jinghewang <jinghewang@163.com>
2016-05-19 08:49:44 +0800

    OnClickListener

--- app/src/main/java/com/hbdworld/ashello/MainActivity.java
+++ app/src/main/java/com/hbdworld/ashello/MainActivity.java
@@ -2,6 +2,11 @@ package com.hbdworld.ashello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //--
+        Button my_btn = (Button) this.findViewById(R.id.my_btn);
+        final TextView tv = (TextView)this.findViewById(R.id.tv);
+        my_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(new Date().toString());
+            }
+        });
+
+
     }
 }
