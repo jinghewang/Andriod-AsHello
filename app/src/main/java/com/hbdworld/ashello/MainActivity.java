6c9556b61071e66d5ee3dc1aff51de535e6c7fa4
jinghewang <jinghewang@163.com>
2016-05-19 13:02:04 +0800

    相册功能

--- app/src/main/java/com/hbdworld/ashello/MainActivity.java
+++ app/src/main/java/com/hbdworld/ashello/MainActivity.java
@@ -6,10 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Date;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] images = new int[]{R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4};
+    private ImageView imageView ;
+    private Integer currentIndex =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         //--
         final Button my_btn = (Button) this.findViewById(R.id.my_btn);
+        final Button btnFirst = (Button)this.findViewById(R.id.btnFirst);
         final Button btnNext = (Button)this.findViewById(R.id.btnNext);
-        final Button btnLast = (Button)this.findViewById(R.id.btgLast);
+        final Button btnPre = (Button)this.findViewById(R.id.btnPre);
+        final Button btnLast = (Button)this.findViewById(R.id.btnLast);
         final TextView tv = (TextView)this.findViewById(R.id.tv);
+        imageView = (ImageView)this.findViewById(R.id.imageView);
 
         my_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,22 +41,40 @@ public class MainActivity extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv.setText(btnNext.getText());
+                currentIndex++;
+                imageView.setImageResource(images[Math.abs(currentIndex)%4]);
+                show(currentIndex.toString());
+            }
+        });
+
+        btnPre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentIndex--;
+                imageView.setImageResource(images[Math.abs(currentIndex)%4]);
+                show(currentIndex.toString());
             }
         });
 
         btnLast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv.setText(btnLast.getText());
+                currentIndex = images.length-1;
+                imageView.setImageResource(images[Math.abs(currentIndex)%4]);
+                show(currentIndex.toString());
             }
         });
+    }
 
+    public void btnFirst_Click(View view) {
+        currentIndex = 0;
+        show(currentIndex.toString());
+        imageView.setImageResource(images[Math.abs(currentIndex)]);
+    }
 
-//        int[] images = new int[R.drawable.img1];
-//        ImageView imageView = (ImageView)this.findViewById(R.id.imageView);
-//        imageView.setImageResource(images[0]);
-
-
+    public void show(String text){
+        final TextView tv = (TextView)this.findViewById(R.id.tv);
+        tv.setText(text);
+        Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT).show();
     }
 }
