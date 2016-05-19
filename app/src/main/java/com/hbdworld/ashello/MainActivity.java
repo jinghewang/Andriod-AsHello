febf999aed453dd01a0fab20c2aabfacce597761
jinghewang <jinghewang@163.com>
2016-05-19 09:49:42 +0800

    添加图片资源

--- app/src/main/java/com/hbdworld/ashello/MainActivity.java
+++ app/src/main/java/com/hbdworld/ashello/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -16,14 +17,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //--
-        Button my_btn = (Button) this.findViewById(R.id.my_btn);
+        final Button my_btn = (Button) this.findViewById(R.id.my_btn);
+        final Button btnNext = (Button)this.findViewById(R.id.btnNext);
+        final Button btnLast = (Button)this.findViewById(R.id.btgLast);
         final TextView tv = (TextView)this.findViewById(R.id.tv);
+
         my_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 tv.setText(new Date().toString());
             }
         });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(btnNext.getText());
+            }
+        });
+
+        btnLast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(btnLast.getText());
+            }
+        });
+
+
+//        int[] images = new int[R.drawable.img1];
+//        ImageView imageView = (ImageView)this.findViewById(R.id.imageView);
+//        imageView.setImageResource(images[0]);
 
 
     }
